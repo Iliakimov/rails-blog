206e75f7e6c209f39860ff355f36c99cb1abf166
dekoyre <dekoyre@gmail.com>
2016-07-19 10:06:38 +0300

    Fix action Create. Add bootstrap.

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,48 +1,29 @@
 jQuery(function ($) {
-    $('.save-article').click(function (event) {
+    $('.create-article').click(function (event) {
         event.preventDefault();
-        var msg   = $('#new_article').serialize();
+        var articleData   = $('#new_article').serialize();
         var articleTitle = $("#article_title").val();
         var articleText = $("#article_text").val();
-        if((articleTitle.length > 4)&(articleText.length > 4)) {
-            $.ajax({
-                url: '/articles',
-                type: 'POST',
-                data: msg,
-                success: function (result) {
-                    console.log(result)
-                },
-                error: function (xhr) {
-                    console.log(xhr.responseCode)
-                }
-            })
-        }
-        else {
-            console.log("error");
-            // $('#errors').css("display", "block");
-            $('#errors').show(300);
-            if (articleTitle.length <= 4) {
-                $('#title_error').show(300)
-            }
-            else {
-                $('#title_error').hide(300)
-            }
-            if (articleText.length <= 4) {
-                $('#text_error').show(300)
-            }
-            else {
-                $('#text_error').hide(300)
-            }
-        }
+        ClickAction (articleTitle, articleText, articleData);
     });
 
+    $('#edit_article').ready( function () {
+        var msg;
+        $.ajax({
+            url: '/articles/:id',
+            type: 'GET',
+            data: msg,
+            succes: function (msg) {
+                console.log(msg);
+            }
+        });
+    })
 
 
-    function ClickFunction () {
-        var msg   = $('form_article').serialize();
-        var articleTitle = $("#article_title").val();
-        var articleText = $("#article_text").val();
-        if((articleTitle.length > 4)&(articleText.length > 4)) {
+
+
+    function ClickAction (title, text, msg) {
+        if((title.length > 4)&(text.length > 4)) {
             $.ajax({
                 url: '/articles',
                 type: 'POST',
@@ -59,13 +40,13 @@ jQuery(function ($) {
             console.log("error");
             // $('#errors').css("display", "block");
             $('#errors').show(300);
-            if (articleTitle.length <= 4) {
+            if (title.length <= 4) {
                 $('#title_error').show(300)
             }
             else {
                 $('#title_error').hide(300)
             }
-            if (articleText.length <= 4) {
+            if (text.length <= 4) {
                 $('#text_error').show(300)
             }
             else {
